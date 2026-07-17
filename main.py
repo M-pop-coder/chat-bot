d37e3a51f70e7e25707120cf069392bf3c6bbaee
M.mohammadpour <287504389+mohammadpourn1390-wq@users.noreply.github.com>
2026-07-17 23:33:50 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ while True:
     user_message = input("👤 شما: ")
 
     # EIXIT LOOP
-    if user_message.lower() == "exit":
+    if user_message.lower().strip() == "exit":
         print("🤖 خداحافظ!")
         break
+    elif user_message.strip() == "خداحافظ":
+        print("سلام من رو به مادرت ابلاغ کن")
+        break
 
-    
     if user_message == "کمک":
         print("چه کمکی از دستم برمیاد؟")
+    if user_message == "ممنون":
+        print("متشکرم.برای خدمت گذاری آماده ام")
     else:
         print("🤖 گفتی:", user_message)
