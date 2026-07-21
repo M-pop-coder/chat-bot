aa120a7395085288290f7c5b20188bb0f0103303
M.mohammadpour <287504389+mohammadpourn1390-wq@users.noreply.github.com>
2026-07-21 23:10:42 +0330

    Create memory.py

---
+++ memory.py
@@ -0,0 +1,15 @@
+def show_history(history):
+
+    if not history:
+        print("📜 هنوز تاریخچه‌ای وجود ندارد.")
+        return
+
+    print("\n📜 تاریخچه گفتگو:\n")
+
+    for message in history:
+
+        if message["role"] == "user":
+            print(f'👤 {message["content"]}')
+
+        else:
+            print(f'🤖 {message["content"]}')
